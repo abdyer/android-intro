f1e5a81580b0e381cef47e4b4c0ab655bbde49ba
Andrew Dyer <abdyer@gmail.com>
2014-03-13 11:42:35 -0500

    Setup bitmap cache and image loader.

--- app/src/main/java/com/example/app/GithubEventsApp.java
+++ app/src/main/java/com/example/app/GithubEventsApp.java
@@ -3,7 +3,9 @@ package com.example.app;
 import android.app.Application;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.example.app.cache.LruBitmapCache;
 
 /**
  * Created by andy on 3/12/14.
@@ -11,14 +13,20 @@ import com.android.volley.toolbox.Volley;
 public class GithubEventsApp extends Application {
 
     private static RequestQueue requestQueue;
+    private static LruBitmapCache bitmapCache;
+    private static ImageLoader imageLoader;
 
     @Override
     public void onCreate() {
         super.onCreate();
         requestQueue = Volley.newRequestQueue(getApplicationContext());
+        bitmapCache = new LruBitmapCache(10 * 1024);
+        imageLoader = new ImageLoader(requestQueue, bitmapCache);
     }
 
     public static RequestQueue getRequestQueue() {
         return requestQueue;
     }
+
+    public static ImageLoader getImageLoader() { return imageLoader; }
 }
---
+++ app/src/main/java/com/example/app/cache/LruBitmapCache.java
@@ -0,0 +1,31 @@
+package com.example.app.cache;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * Created by andy on 3/13/14.
+ */
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache {
+
+    public LruBitmapCache(int maxSize) {
+        super(maxSize);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight();
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
