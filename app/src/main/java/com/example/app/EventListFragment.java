3b0e15b9125524c999365e087a66ed95b9f878ba
Andrew Dyer <abdyer@gmail.com>
2014-03-13 11:43:18 -0500

    Implement custom list item layout with view holder and async image loading.

--- app/src/main/java/com/example/app/EventListFragment.java
+++ app/src/main/java/com/example/app/EventListFragment.java
@@ -3,6 +3,7 @@ package com.example.app;
 import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,20 @@ import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.NetworkImageView;
 import com.example.app.api.Event;
 import com.example.app.api.EventsResponse;
 import com.example.app.api.GitHubRequest;
 import com.example.app.api.GitHubRequestManager;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by andy on 3/12/14.
@@ -40,8 +49,7 @@ public class EventListFragment extends ListFragment implements Response.Listener
     public void onResponse(EventsResponse response) {
         getActivity().setProgressBarIndeterminateVisibility(false);
         setListShown(true);
-        EventsAdapter adapter = new EventsAdapter(getActivity(),
-                android.R.layout.simple_list_item_2, response);
+        EventsAdapter adapter = new EventsAdapter(getActivity(), response);
         getListView().setAdapter(adapter);
     }
 
@@ -53,23 +61,56 @@ public class EventListFragment extends ListFragment implements Response.Listener
 
     private class EventsAdapter extends ArrayAdapter<Event> {
 
-        public EventsAdapter(Context context, int resource, List<Event> objects) {
-            super(context, resource, objects);
+        public EventsAdapter(Context context, List<Event> objects) {
+            super(context, -1, objects);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
             if(convertView == null) {
                 convertView = LayoutInflater.from(getContext())
-                        .inflate(android.R.layout.simple_list_item_2, parent, false);
+                        .inflate(R.layout.event_list_item, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }
+            else {
+                holder = ViewHolder.class.cast(convertView.getTag());
             }
 
             Event event = getItem(position);
             String line1 = String.format("%s by %s", event.getType(), event.getActor().getLogin());
-            TextView.class.cast(convertView.findViewById(android.R.id.text1)).setText(line1);
-            TextView.class.cast(convertView.findViewById(android.R.id.text2)).setText(event.getCreatedAt());
+            holder.textLine1.setText(line1);
+            holder.textLine2.setText(getRelativeTime(event.getCreatedAt()));
+            holder.avatar.setImageUrl(event.getActor().getAvatarUrl(), GithubEventsApp.getImageLoader());
 
             return convertView;
+        }
+
+        private String getRelativeTime(String isoDateTime) {
+            try {
+                TimeZone utc = TimeZone.getTimeZone("UTC");
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                format.setTimeZone(utc);
+                GregorianCalendar calendar = new GregorianCalendar(utc);
+                calendar.setTime(format.parse(isoDateTime));
+                return DateUtils.getRelativeTimeSpanString(calendar.getTimeInMillis(),
+                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            } catch (ParseException e) {
+                return isoDateTime;
+            }
+        }
+    }
+
+    class ViewHolder {
+
+        @InjectView(R.id.event_list_item_avatar) NetworkImageView avatar;
+        @InjectView(R.id.event_list_item_text_line1) TextView textLine1;
+        @InjectView(R.id.event_list_item_text_line2) TextView textLine2;
+
+
+        public ViewHolder(View view) {
+            ButterKnife.inject(this, view);
         }
     }
 }
