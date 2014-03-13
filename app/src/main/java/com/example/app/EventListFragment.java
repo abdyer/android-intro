093f8d73f123c91ddcc109565ee965be971cb4fa
Andrew Dyer <abdyer@gmail.com>
2014-03-13 09:40:30 -0500

    Add custom adapter for events list.

--- app/src/main/java/com/example/app/EventListFragment.java
+++ app/src/main/java/com/example/app/EventListFragment.java
@@ -1,9 +1,13 @@
 package com.example.app;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
@@ -12,6 +16,8 @@ import com.example.app.api.Event;
 import com.example.app.api.EventsResponse;
 import com.example.app.api.GitHubRequest;
 import com.example.app.api.GitHubRequestManager;
+
+import java.util.List;
 
 /**
  * Created by andy on 3/12/14.
@@ -34,8 +40,8 @@ public class EventListFragment extends ListFragment implements Response.Listener
     public void onResponse(EventsResponse response) {
         getActivity().setProgressBarIndeterminateVisibility(false);
         setListShown(true);
-        ArrayAdapter<Event> adapter = new ArrayAdapter<Event>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, response);
+        EventsAdapter adapter = new EventsAdapter(getActivity(),
+                android.R.layout.simple_list_item_2, response);
         getListView().setAdapter(adapter);
     }
 
@@ -43,5 +49,27 @@ public class EventListFragment extends ListFragment implements Response.Listener
     public void onErrorResponse(VolleyError volleyError) {
         getActivity().setProgressBarIndeterminateVisibility(false);
         Toast.makeText(getActivity(), R.string.unable_to_fech_events, Toast.LENGTH_SHORT).show();
+    }
+
+    private class EventsAdapter extends ArrayAdapter<Event> {
+
+        public EventsAdapter(Context context, int resource, List<Event> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = LayoutInflater.from(getContext())
+                        .inflate(android.R.layout.simple_list_item_2, parent, false);
+            }
+
+            Event event = getItem(position);
+            String line1 = String.format("%s by %s", event.getType(), event.getActor().getLogin());
+            TextView.class.cast(convertView.findViewById(android.R.id.text1)).setText(line1);
+            TextView.class.cast(convertView.findViewById(android.R.id.text2)).setText(event.getCreatedAt());
+
+            return convertView;
+        }
     }
 }
